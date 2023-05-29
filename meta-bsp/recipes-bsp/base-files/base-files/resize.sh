0bbd4061a18b71bd5e70a81043db598223c3401d
BelugaHiL <beluga@cetasol.com>
2023-05-29 11:38:18 +0200

    Do not resize when using SSH to connect
     - The resize command sets the terminal size for the current session,
       which includes the number of rows and columns that are displayed
       in the terminal window. This messes up our Bash commands input
       when the line exceeds the line length. Keep the settings when
       accessing the machine via serial, but avoid for SSH connections.

--- meta-bsp/recipes-bsp/base-files/base-files/resize.sh
+++ meta-bsp/recipes-bsp/base-files/base-files/resize.sh
@@ -1,2 +1,5 @@
-shopt -s checkwinsize
-command -v resize &>/dev/null && resize
+if [ -z "$SSH_CLIENT" ] && [ -z "$SSH_TTY" ]; then
+    # We're not running over SSH connection, i.e. in a local terminal, so run the resize command
+    shopt -s checkwinsize
+    command -v resize &>/dev/null && resize
+fi
